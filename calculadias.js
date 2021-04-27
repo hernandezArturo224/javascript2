b43a92e4bb559a9b6e6ebed39575692675fdef0c
Arturo Hernandez Nuñez <arturohernandez1@usal.es>
2021-04-27 10:11:13 +0200

    Realizado el cambio de dias en funcion del mes y el año

--- calculadias.js
+++ calculadias.js
@@ -3,5 +3,30 @@ function calculaDias(){
 	var agno = document.getElementById("agno").value;
 
 
-	alert("Dia cambiado..."+mes+" "+agno);
+	if(mes == 'febrero'){
+		document.getElementById("op31").style.display = 'none';
+		document.getElementById("op30").style.display = 'none';
+		if(!esBisiesto(agno)){
+			document.getElementById("op29").style.display = 'none';
+		}else{
+			document.getElementById("op29").style.display = 'block';
+		}
+	}else if(mes == 'abril' || mes == 'junio' || mes == 'septiembre' || mes == 'noviembre'){
+		document.getElementById("op31").style.display = 'none';
+	}else{
+		document.getElementById("op31").style.display = 'block';
+		document.getElementById("op30").style.display = 'block';
+		document.getElementById("op29").style.display = 'block';
+	}
+
+	//alert("Dia cambiado..."+mes+" "+agno);
+}
+
+function esBisiesto(agno){
+	if((agno%4 == 0 && agno%100 != 0) || agno%400 == 0){
+		return true;
+	}else{
+		return false;
+	}
+
 }
